372e738457c2250e2effea3d54d5f88e1d375390
xu han <xhan87@gmail.com>
2010-12-23 22:00:21 +0800

    add more NS+UI categories

---
+++ NSDictionary+URL.m
@@ -0,0 +1,25 @@
+//
+//  NSDictionary+URL.m
+//  Apollo
+//
+//  Created by xhan on 12/16/10.
+//  Copyright 2010 Baidu.com. All rights reserved.
+//
+
+#import "NSDictionary+URL.h"
+
+
+@implementation NSDictionary(URL)
+
+
+- (NSString*)toURLarguments
+{
+	NSMutableArray* args = [NSMutableArray arrayWithCapacity:[self count]];
+	for (NSString* key in [self allKeys]) {
+		[args addObject:[NSString stringWithFormat:
+						 @"%@=%@",key, [[self objectForKey:key] description]]];		
+	}
+	return [args componentsJoinedByString:@"&"];
+}
+
+@end
