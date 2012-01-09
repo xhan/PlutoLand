397e629134da489509b20e4b2fc0734bd49518ee
xu han <xhan87@gmail.com>
2012-01-09 17:17:52 +0800

    added nsnumber addition

---
+++ PlutoLand/Categories/NSNumber+PLAddition.h
@@ -0,0 +1,13 @@
+//
+//  NSNumber+PLAddition.h
+//  QiuBai
+//
+//  Created by xu xhan on 1/6/12.
+//  Copyright (c) 2012 Less Everything. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (PLAddition)
+
+@end
---
+++ PlutoLand/Categories/NSNumber+PLAddition.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+PLAddition.m
+//  QiuBai
+//
+//  Created by xu xhan on 1/6/12.
+//  Copyright (c) 2012 Less Everything. All rights reserved.
+//
+
+#import "NSNumber+PLAddition.h"
+
+@implementation NSNumber (PLAddition)
+
+@end
