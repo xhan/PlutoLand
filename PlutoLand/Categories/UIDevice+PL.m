26bf17ece079fa754eabe5dac7a304ad38ab11c1
xu han <xhan87@gmail.com>
2012-03-26 23:59:56 +0800

    added new uuid implement to UIDevice

--- PlutoLand/Categories/UIDevice+PL.m
+++ PlutoLand/Categories/UIDevice+PL.m
@@ -98,3 +98,95 @@
 
 
 @end
+
+
+#include <sys/socket.h> // Per msqr
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#import "NSString+Addition.h"
+@interface UIDevice(Private)
+
+- (NSString *) macaddress;
+
+@end
+
+@implementation UIDevice (IdentifierAddition)
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Private Methods
+
+// Return the local MAC addy
+// Courtesy of FreeBSD hackers email list
+// Accidentally munged during previous update. Fixed thanks to erica sadun & mlamb.
+- (NSString *) macaddress{
+    
+    int                 mib[6];
+    size_t              len;
+    char                *buf;
+    unsigned char       *ptr;
+    struct if_msghdr    *ifm;
+    struct sockaddr_dl  *sdl;
+    
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    
+    if ((mib[5] = if_nametoindex("en0")) == 0) {
+        printf("Error: if_nametoindex error\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 1\n");
+        return NULL;
+    }
+    
+    if ((buf = malloc(len)) == NULL) {
+        printf("Could not allocate memory. error!\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 2");
+        free(buf);
+        return NULL;
+    }
+    
+    ifm = (struct if_msghdr *)buf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
+    NSString *outstring = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X", 
+                           *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    free(buf);
+    
+    return outstring;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Public Methods
+
+- (NSString *) uniqueDeviceIdentifier{
+    NSString *macaddress = [[UIDevice currentDevice] macaddress];
+    NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+    
+    NSString *stringToHash = [NSString stringWithFormat:@"%@%@",macaddress,bundleIdentifier];
+    NSString *uniqueIdentifier = [stringToHash md5];
+    
+    return uniqueIdentifier;
+}
+
+- (NSString *) uniqueGlobalDeviceIdentifier{
+    NSString *macaddress = [[UIDevice currentDevice] macaddress];
+    NSString *uniqueIdentifier = [macaddress md5];
+    
+    return uniqueIdentifier;
+}
+
+@end
+
+
